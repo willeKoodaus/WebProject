53296014c85d9ff7fa0eb0b86b84c6b8baff2eb0
willeKoodaus <willep@metropolia.fi>
2022-12-05 19:41:13 +0200

    Saatu sovelluksen prototyyppi yhdistettyä MongoDB

--- server/index.js
+++ server/index.js
@@ -32,7 +32,7 @@ mongoose.connect(process.env.MONGO_URI)
   .then(() => {
     // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log('connected to db & listening on port', process.env.PORT || 4000)
+      console.log('connected to db & listening on port', process.env.PORT || 3000)
     })
   })
   .catch((error) => {

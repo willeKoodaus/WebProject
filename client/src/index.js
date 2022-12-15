3cb7ae08c27a60dbd4828b055cbb1a62e663909a
willeKoodaus <willep@metropolia.fi>
2022-12-15 20:23:08 +0200

    fixed some routing problems for deployment

--- client/src/index.js
+++ client/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-
+import { BrowserRouter } from 'react-router-dom';
 
 import App from './App';
 import { RecipesContextProvider } from './context/RecipeContext'
@@ -10,10 +10,12 @@ import { AuthContextProvider } from './context/AuthContext'
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter basename={"/client"}>
     <AuthContextProvider>
       <RecipesContextProvider>
         <App />
       </RecipesContextProvider>
     </AuthContextProvider>
+    </BrowserRouter>
   </React.StrictMode>
 );
